2f2b6850bd985421a3c330b33916caaa56477943
xia0sheng <wangyouyu6@163.com>
2025-01-09 16:46:07 +0800

    增加静态大屏展示功能，不涉及动态数据

--- admin-ui/src/components/Sidebar.vue
+++ admin-ui/src/components/Sidebar.vue
@@ -1,5 +1,13 @@
-{
-  title: '大屏展示',
-  icon: 'monitor-outline',  // 或其他适合的图标
-  to: '/big-screen'
+export default {
+  data() {
+    return {
+      menuItems: [
+        {
+          title: '大屏展示',
+          icon: 'monitor-outline',
+          to: '/admin/big-screen'
+        }
+      ]
+    }
+  }
 } 
--- admin-ui/src/router/index.js
+++ admin-ui/src/router/index.js
@@ -1,1 +1,9 @@
- 
+{
+  path: '/admin/big-screen',
+  name: 'BigScreen',
+  component: BigScreen,
+  meta: {
+    requiresAuth: true,
+    title: '大屏展示'
+  }
+} 
